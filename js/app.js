74742ec2c5a24627de4796a0413538e2f6c22073
TURKI-Alqasem <trk-96@hotmail.com>
2018-10-31 14:59:50 +0300

    need the pull

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 
   document.getElementById("Boyplus").addEventListener("click", function() {
     console.log("Boyplus ");
-    document.getElementById("Boyinput").value ++;
+    
+        document.getElementById("Boyinput").value++;
+
+     
   });
   document.getElementById("Boyminus").addEventListener("click", function() {
     console.log("Boyminus ");
-    document.getElementById("Boyinput").value --;
+    if(document.getElementById("Boyinput").value != 0){
+        document.getElementById("Boyinput").value--;
+    }
+     
 
   });
   document.getElementById("girlplus").addEventListener("click", function() {
     console.log("girlplus ");
-    document.getElementById("girlsinput").value ++;
+    
+        document.getElementById("girlsinput").value++;
+         
 
   });
   document.getElementById("girlminus").addEventListener("click", function() {
     console.log("girlminus ");
-    document.getElementById("girlsinput").value --;
+    if(document.getElementById("girlsinput").value  != 0){
+        document.getElementById("girlsinput").value --;
+    }
+    
 
   });
